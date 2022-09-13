d3cc2e1fb6f703a5738e1ecc63e4320d83180c76
1mustyz <onemusty.z@gmail.com>
2022-09-13 05:59:10 -0700

    comit

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -10,7 +10,11 @@ int main(void)
 {
 	int r;
 
-	r = _abs(-1);
-	printf("%d\n", r);
+		
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
+	_putchar('\n');
 	return (0);
 }
