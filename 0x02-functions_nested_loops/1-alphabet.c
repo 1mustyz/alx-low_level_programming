af8272b0c0e46730d8f165fe5352b00eed2989d9
1mustyz <onemusty.z@gmail.com>
2022-09-13 04:32:01 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-
-/**
  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
 
