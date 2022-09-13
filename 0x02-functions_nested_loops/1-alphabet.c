e5720c2ec3fe4386a6a5b91187891a1c1ca1e2b4
1mustyz <onemusty.z@gmail.com>
2022-09-13 04:27:00 -0700

    alpahbet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
